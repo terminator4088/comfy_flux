b3912b6bf1d9d7ffd4d1a1cd9c701f42be550f45
terminator4088 <43856573+terminator4088@users.noreply.github.com>
2024-08-21 18:04:52 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,7 +14,10 @@ cd SimpleTuner
 python3 -m venv .venv
 source .venv/bin/activate
 pip install -U poetry pip
+
+# Install Dependencie
 pip install huggingface_hub
+pip install wandb
 
 # Log Into Huggingface
 huggingface-cli login --token $HFTK --add-to-git-credential
@@ -30,7 +33,7 @@ pip uninstall diffusers
 pip install git+https://github.com/huggingface/diffusers
 
 #
-cat >config/config.env <<'EOF'
+cat >config/config.env <<EOF
 export MODEL_TYPE='lora'
 
 export MODEL_NAME="black-forest-labs/FLUX.1-dev"
@@ -62,7 +65,12 @@ export PUSH_TO_HUB="true"
 export PUSH_CHECKPOINTS="true"
 # This will be the model name for your final hub upload, eg. "yourusername/yourmodelname"
 # It defaults to the wandb project name, but you can override this here.
-export HUB_MODEL_NAME=$TRACKER_PROJECT_NAME
+export HUB_MODEL_NAME=$SUBJECT
+
+# Make DEBUG_EXTRA_ARGS empty to disable wandb.
+export DEBUG_EXTRA_ARGS="--report_to=wandb"
+export TRACKER_PROJECT_NAME="$SUBJECT-training"
+export TRACKER_RUN_NAME="simpletuner-fluxx"
 
 export RESOLUTION=1024
 export RESOLUTION_TYPE="pixel"
@@ -155,8 +163,8 @@ export TRAINER_EXTRA_ARGS=""
 export TRAINER_EXTRA_ARGS="${TRAINER_EXTRA_ARGS} --i_know_what_i_am_doing"
 export TRAINER_EXTRA_ARGS="${TRAINER_EXTRA_ARGS} --lora_rank=4"
 export TRAINER_EXTRA_ARGS="${TRAINER_EXTRA_ARGS} --max_grad_norm=1.0 --gradient_precision=fp32"
-# x-flux only trains the mmdit blocks but you can change lora_target to all or context to experiment.
 export TRAINER_EXTRA_ARGS="${TRAINER_EXTRA_ARGS} --base_model_default_dtype=bf16 --lora_init_type=default --flux_lora_target=mmdit"
+export TRAINER_EXTRA_ARGS="${TRAINER_EXTRA_ARGS} --user_prompt_library=config/user_prompt_library.json"
 
 # Reproducible training. Set to -1 to disable.
 export TRAINING_SEED=42
@@ -180,7 +188,26 @@ EOF
 cat > config/multidatabackend.json <<EOF
 [
   {
-    "id": "blackswan-flux",
+    "id": "pseudo-camera-10k-flux",
+    "type": "local",
+    "crop": true,
+    "crop_aspect": "square",
+    "crop_style": "center",
+    "resolution": 512,
+    "minimum_image_size": 512,
+    "maximum_image_size": 512,
+    "target_downsample_size": 512,
+    "resolution_type": "pixel_area",
+    "cache_dir_vae": "/workspace/SimpleTuner/cache/vae/flux/pseudo-camera-10k",
+    "instance_data_dir": "/workspace/SimpleTuner/datasets/pseudo-camera-10k",
+    "ignore_epochs": true,
+    "disabled": false,
+    "skip_file_discovery": "",
+    "caption_strategy": "filename",
+    "metadata_backend": "json"
+  },
+  {
+    "id": "$SUBJECT-flux",
     "type": "local",
     "crop": false,
     "crop_aspect": "preserve",
@@ -190,8 +217,8 @@ cat > config/multidatabackend.json <<EOF
     "maximum_image_size": 512,
     "target_downsample_size": 512,
     "resolution_type": "pixel",
-    "cache_dir_vae": "cache/vae/flux/blackswan",
-    "instance_data_dir": "/workspace/SimpleTuner/output/blackswan/dataset/",
+    "cache_dir_vae": "/workspace/SimpleTuner/cache/vae/flux/$SUBJECT",
+    "instance_data_dir": "/workspace/SimpleTuner/output/$SUBJECT/dataset/",
     "disabled": false,
     "skip_file_discovery": "",
     "caption_strategy": "textfile",
@@ -202,158 +229,37 @@ cat > config/multidatabackend.json <<EOF
     "type": "local",
     "dataset_type": "text_embeds",
     "default": true,
-    "cache_dir": "cache/text/flux/blackswan",
+    "cache_dir": "/workspace/SimpleTuner/cache/text/flux/blackswan",
     "disabled": false,
     "write_batch_size": 128
   }
 ]
 EOF
 
-cd /workspace
+cat > config/user_prompt_library.json <<EOF
+{
+    "anime_$SUBJECT": "a breathtaking anime-style portrait of $SUBJECT, capturing her essence with vibrant colors and expressive features",
+    "chef_$SUBJECT": "a high-quality, detailed photograph of $SUBJECT as a sous-chef, immersed in the art of culinary creation",
+    "just_$SUBJECT": "a lifelike and intimate portrait of $SUBJECT, showcasing her unique personality and charm",
+    "cinematic_$SUBJECT": "a cinematic, visually stunning photo of $SUBJECT, emphasizing her dramatic and captivating presence",
+    "elegant_$SUBJECT": "an elegant and timeless portrait of $SUBJECT, exuding grace and sophistication",
+    "adventurous_$SUBJECT": "a dynamic and adventurous photo of $SUBJECT, captured in an exciting, action-filled moment",
+    "mysterious_$SUBJECT": "a mysterious and enigmatic portrait of $SUBJECT, shrouded in shadows and intrigue",
+    "vintage_$SUBJECT": "a vintage-style portrait of $SUBJECT, evoking the charm and nostalgia of a bygone era",
+    "artistic_$SUBJECT": "an artistic and abstract representation of $SUBJECT, blending creativity with visual storytelling",
+    "futuristic_$SUBJECT": "a futuristic and cutting-edge portrayal of $SUBJECT, set against a backdrop of advanced technology",
+    "woman": "a beautifully crafted portrait of a woman, highlighting her natural beauty and unique features",
+    "man": "a powerful and striking portrait of a man, capturing his strength and character",
+    "boy": "a playful and spirited portrait of a boy, capturing youthful energy and innocence",
+    "girl": "a charming and vibrant portrait of a girl, emphasizing her bright personality and joy",
+    "family": "a heartwarming and cohesive family portrait, showcasing the bonds and connections between loved ones"
+}
+EOF
 
 
-
+# Regularization Images
 apt -y install git-lfs
+mkdir -p datasets
+git clone https://huggingface.co/datasets/ptx0/pseudo-camera-10k  datasets/pseudo-camera-10k
 
-##Download orig_backup
-apt -y install rclone
-
-##Fresh SD Install
-mkdir /workspace/stable-diffusion-webui
-cd /workspace/stable-diffusion-webui
-
-if [ -z "$A1111" ]; then
-	echo "Installing VLAD"
-	git clone https://github.com/vladmandic/automatic.git ./
-else
-	echo "Installing A1111"
-	git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui.git ./
- 	#git checkout tags/v1.6.0
-
-  	cd extensions
-	git clone https://github.com/Mikubill/sd-webui-controlnet.git
-	#git clone https://github.com/ahgsql/StyleSelectorXL
-	#git clone https://github.com/imrayya/stable-diffusion-webui-Prompt_Generator.git
-	#git clone https://github.com/IDEA-Research/DWPose
-	git clone https://github.com/Uminosachi/sd-webui-inpaint-anything.git
-	#git clone https://github.com/d8ahazard/sd_dreambooth_extension.git ./extensions/sd_dreambooth_extension
-fi
-
-if [ -z "$A1111" ]; then
-	controlnet_path='/workspace/stable-diffusion-webui/extensions-builtin/sd-webui-controlnet/models/'
-else
-	controlnet_path='/workspace/stable-diffusion-webui/extensions/sd-webui-controlnet/models/'
-fi
-
-(
-wget "https://huggingface.co/lllyasviel/sd_control_collection/resolve/main/diffusers_xl_depth_mid.safetensors" -P $controlnet_path ;
-wget "https://huggingface.co/lllyasviel/sd_control_collection/resolve/main/diffusers_xl_canny_mid.safetensors" -P $controlnet_path ;
-wget "https://huggingface.co/lllyasviel/sd_control_collection/resolve/main/ip-adapter_xl.pth" -P $controlnet_path ;
-wget "https://huggingface.co/lllyasviel/sd_control_collection/resolve/main/kohya_controllllite_xl_blur.safetensors" -P $controlnet_path ;
-wget "https://huggingface.co/lllyasviel/sd_control_collection/resolve/main/t2i-adapter_xl_openpose.safetensors" -P $controlnet_path ;
-wget "https://huggingface.co/lllyasviel/sd_control_collection/resolve/main/thibaud_xl_openpose_256lora.safetensors" -P $controlnet_path ;
-wget "https://huggingface.co/lllyasviel/sd_control_collection/resolve/main/sai_xl_sketch_256lora.safetensors" -P $controlnet_path ;
-wget "https://huggingface.co/lllyasviel/sd_control_collection/resolve/main/sai_xl_recolor_256lora.safetensors" -P $controlnet_path ;
-wget "https://huggingface.co/lllyasviel/sd_control_collection/resolve/main/sai_xl_depth_256lora.safetensors" -P $controlnet_path ;
-wget "https://huggingface.co/lllyasviel/sd_control_collection/resolve/main/sai_xl_canny_256lora.safetensors" -P $controlnet_path ;
-wget "https://huggingface.co/h94/IP-Adapter-FaceID/resolve/main/ip-adapter-faceid_sdxl.bin" -P $controlnet_path ;
-
-sleep 10
-touch /workspace/download/control_finish) &> control_download.log &
-
-(
-cd /workspace/stable-diffusion-webui/models;
-mkdir Stable-diffusion;
-mkdir Lora;
-mkdir embeddings;
-mkdir VAE;
-wget "https://civitai.com/api/download/models/198246?type=Model&format=SafeTensor&size=pruned&fp=fp16" -O "Stable-diffusion/TimeLessXL.safetensors";
-wget "https://civitai.com/api/download/models/169740?type=Model&format=SafeTensor&size=full&fp=fp16" -O "Stable-diffusion/ZavyXL.safetensors";
-wget "https://huggingface.co/segmind/SSD-1B/resolve/main/SSD-1B-A1111.safetensors?download=true" -O "Stable-diffusion/SSD-1B.safetensors";
-
-wget "https://civitai.com/api/download/models/131960?type=VAE&format=SafeTensor" -O "VAE/TalmendoXL.safetensors";
-
-wget "https://huggingface.co/latent-consistency/lcm-lora-sdxl/resolve/main/pytorch_lora_weights.safetensors?download=true" -O "Lora/LCM-SDXL.safetensors";
-wget "https://huggingface.co/latent-consistency/lcm-lora-ssd-1b/resolve/main/pytorch_lora_weights.safetensors?download=true" -O "Lora/LCM-SSD-1B.safetensors";
-
-wget "https://civitai.com/api/download/models/135867?type=Model&format=SafeTensor" -O "Lora/Detail_Tweaker_XL.safetensors" ;
-wget "https://civitai.com/api/download/models/169002?type=Model&format=SafeTensor" -O "Lora/Artfull_Fractal.safetensors" ;
-wget "https://civitai.com/api/download/models/152309?type=Model&format=SafeTensor" -O "Lora/Artfull_Base.safetensors" ;
-wget "https://civitai.com/api/download/models/169041?type=Model&format=SafeTensor" -O "Lora/Schematics.safetensors" ;
-
-
-sleep 10
-touch /workspace/download/finish) &> download.log &
-
-#Define Copy Job
-(cd /workspace
-mkdir stable-diffusion-webui/models/Stable-diffusion/
-mkdir stable-diffusion-webui/models/embeddings/
-mkdir stable-diffusion-webui/models/VAE/
-mkdir stable-diffusion-webui/models/Lora/
-
-if [ -z "$A1111" ]; then
-	controlnet_path='stable-diffusion-webui/extensions-builtin/sd-webui-controlnet/models/'
-else
-	controlnet_path='stable-diffusion-webui/extensions/sd-webui-controlnet/models/'
-fi
-
-while [ ! -f /workspace/download/finish ]; do
-	mv download/Stable-diffusion/* stable-diffusion-webui/models/Stable-diffusion/ &
-	mv download/embeddings/* stable-diffusion-webui/models/embeddings/ &
-	mv download/Lora/* stable-diffusion-webui/models/Lora/ &
-	mv download/VAE/* stable-diffusion-webui/models/VAE/ &
-done
-
-while [ ! -f /workspace/download/control_finish ]; do
-	if [ -d $controlnet_path ]; then
-		mv download/controlnet_models/* $controlnet_path &
-	fi
-
-	sleep 4
-done
-) &> copy.log &
-
-
-#Write necessary files
-cd /workspace/stable-diffusion-webui
-cat  <<EOT > relauncher.py
-#!/usr/bin/python3
-import os, time
-
-n = 0
-while True:
-    print('Relauncher: Launching...')
-    if n > 0:
-        print(f'\tRelaunch count: {n}')
-    launch_string = "/workspace/stable-diffusion-webui/webui.sh -f --listen"
-    os.system(launch_string)
-    print('Relauncher: Process is ending. Relaunching in 2s...')
-    n += 1
-    time.sleep(2)
-EOT
-chmod +x relauncher.py
-
-#source /workspace/venv/bin/activate
-if [ -z "$A1111" ]; then
-	python3 -u /workspace/stable-diffusion-webui/relauncher.py | while IFS= read -r line
-	do
-		echo "--$line"
-		if [[ "$line" == *"Available VAEs"* ]]; then
-			pkill relauncher.py
-			echo "Killed Relauncher as it was stuck at no models"
-			
-			while [[ ! -e /workspace/download/download_finished ]];do
-				sleep 1
-			done
-			
-			/workspace/copy_downloaded_models.sh
-			echo "Copied Models"
-	
-			echo "Setup finished, launching SD :)"  
-			python3 /workspace/stable-diffusion-webui/relauncher.py
-		fi
-	done
-else
-	python3 -u /workspace/stable-diffusion-webui/relauncher.py
-fi
+cd /workspace/SimpleTuner
