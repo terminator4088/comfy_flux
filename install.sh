e13c9be3878caa6d4caa6cf4a8b4335d16aa703c
terminator4088 <43856573+terminator4088@users.noreply.github.com>
2024-09-14 16:45:15 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,18 +12,18 @@ cd /workspace
 ### Downloads
 mkdir downloads
 cd downloads
-(huggingface-cli download comfyanonymous/flux_text_encoders t5xxl_fp8_e4m3fn.safetensors
-huggingface-cli download black-forest-labs/FLUX.1-dev text_encoder/model.safetensors vae/diffusion_pytorch_model.safetensors
-touch /workspace/hf_download_1.fin) &> hf_download_1.log
+(huggingface-cli download comfyanonymous/flux_text_encoders t5xxl_fp8_e4m3fn.safetensors --local-dir /workspace/downloads
+huggingface-cli download black-forest-labs/FLUX.1-dev text_encoder/model.safetensors vae/diffusion_pytorch_model.safetensors --local-dir /workspace/downloads
+touch /workspace/downloads/hf_download_1.fin) &> hf_download_1.log &
 
-(huggingface-cli download black-forest-labs/FLUX.1-dev flux1-dev.safetensors
-touch /workspace/hf_download_2.fin) &> hf_download_2.log
+(huggingface-cli download black-forest-labs/FLUX.1-dev flux1-dev.safetensors --local-dir /workspace/downloads
+touch /workspace/downloads/hf_download_2.fin) &> hf_download_2.log &
 
-(huggingface-cli download XLabs-AI/flux-RealismLora lora.safetensors
-touch /workspace/hf_download_3.fin) &> hf_download_3.log
+(huggingface-cli download XLabs-AI/flux-RealismLora lora.safetensors --local-dir /workspace/downloads
+touch /workspace/downloads/hf_download_3.fin) &> hf_download_3.log &
 
-(huggingface-cli download InstantX/FLUX.1-dev-Controlnet-Union diffusion_pytorch_model.safetensors
-touch /workspace/hf_download_4.fin) &> hf_download_4.log
+(huggingface-cli download InstantX/FLUX.1-dev-Controlnet-Union diffusion_pytorch_model.safetensors --local-dir /workspace/downloads
+touch /workspace/downloads/hf_download_4.fin) &> hf_download_4.log &
 
 cd /workspace
 
