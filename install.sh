41408cb647b6c3315fde065a5d5b6f88cab02660
terminator4088 <43856573+terminator4088@users.noreply.github.com>
2024-09-24 18:02:40 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,69 +13,120 @@ cd /workspace
 ### Downloads
 mkdir downloads
 cd downloads
+
 mkdir text_encoder
 mkdir vae
 mkdir unet
 mkdir lora
 mkdir controlnet
+mkdir llm_gguf
 
-(huggingface-cli download comfyanonymous/flux_text_encoders t5xxl_fp8_e4m3fn.safetensors --local-dir /workspace/downloads
-huggingface-cli download black-forest-labs/FLUX.1-dev text_encoder/model.safetensors vae/diffusion_pytorch_model.safetensors --local-dir /workspace/downloads
-mv t5xxl_fp8_e4m3fn.safetensors text_encoder/t5xxl_fp8_e4m3fn.safetensors
-mv text_encoder/model.safetensors text_encoder/clip_l.safetensors
-mv vae/diffusion_pytorch_model.safetensors vae/flux_vae.safetensors
-touch /workspace/downloads/hf_download_1.fin) &> hf_download_1.log &
+downloads=(
+    ("comfyanonymous/flux_text_encoders" "t5xxl_fp8_e4m3fn.safetensors" "text_encoder/t5xxl_fp8_e4m3fn.safetensors")
+    ("black-forest-labs/FLUX.1-dev" "text_encoder/model.safetensors" "text_encoder/clip_l.safetensors")
+    ("black-forest-labs/FLUX.1-dev" "vae/diffusion_pytorch_model.safetensors" "vae/flux_vae.safetensors")
+    ("black-forest-labs/FLUX.1-dev" "flux1-dev.safetensors" "unet/flux1-dev.safetensor")
+    ("black-forest-labs/FLUX.1-dev" "ae.safetensors" "vae/ae.safetensor")
+    ("XLabs-AI/flux-RealismLora" "lora.safetensors" "lora/realism.safetensors")
+    ("InstantX/FLUX.1-dev-Controlnet-Union" "diffusion_pytorch_model.safetensors" "controlnet/Controlnet-Union.safetensors")
+    ("cognitivecomputations/dolphin-2.9.4-llama3.1-8b-gguf" "dolphin-2.9.4-llama3.1-8b-Q6_K.gguf" "llm_gguf/dolphin-2.9.4-llama3.1-8b-Q6_K.gguf")   
+)
 
-(huggingface-cli download black-forest-labs/FLUX.1-dev flux1-dev.safetensors ae.safetensors --local-dir /workspace/downloads/unet
-mv flux1-dev.safetensors unet/flux1-dev.safetensor
-mv ae.safetensors vae/ae.safetensor
-touch /workspace/downloads/hf_download_2.fin) &> hf_download_2.log &
+download_repo_files() {
+    local repo=$1
+    local file=$2
+    local new_location=$3
 
-(huggingface-cli download XLabs-AI/flux-RealismLora lora.safetensors --local-dir /workspace/downloads
-mv lora.safetensors lora/realism.safetensors
-touch /workspace/downloads/hf_download_3.fin) &> hf_download_3.log &
+    local dest_dir="/workspace/downloads"
+    local folder=$(dirname "$new_location")
 
-(huggingface-cli download InstantX/FLUX.1-dev-Controlnet-Union diffusion_pytorch_model.safetensors --local-dir /workspace/downloads/controlnet
-mv controlnet/diffusion_pytorch_model.safetensors controlnet/Controlnet-Union.safetensors
-touch /workspace/downloads/hf_download_4.fin) &> hf_download_4.log &
+    mkdir -p "$dest_dir/$folder"
+    huggingface-cli download "$repo" "$file" --local-dir "$dest_dir" && mv "$dest_dir/$file" "$dest_dir/$new_location"
+}
 
-cd /workspace
+limit=3
+current_jobs=0
+
+for ((i=0; i<${#downloads[@]}; i++)); do
+    entry=("${downloads[$i]}")
+    repo_path="${entry[0]}"
+    file_name="${entry[1]}"
+    new_location="${entry[2]}"
+
+    echo "Downloading: $file_name"
+    download_repo_files "$repo_path" "$file_name" "$new_location" &
+
+    ((current_jobs+=1))
+    if ((current_jobs >= limit)); then
+        wait -n
+        ((current_jobs-=1))
+    fi
+done
+
+{
+    while jobs %% &>/dev/null; do
+        wait -n
+    done
+
+    touch /workspace/download.fin
+} & # Wait for downloads to finish
+
+#mkdir text_encoder
+#mkdir vae
+#mkdir unet
+#mkdir lora
+#mkdir controlnet
+#
+#(huggingface-cli download comfyanonymous/flux_text_encoders t5xxl_fp8_e4m3fn.safetensors --local-dir /workspace/downloads
+#huggingface-cli download black-forest-labs/FLUX.1-dev text_encoder/model.safetensors vae/diffusion_pytorch_model.safetensors --local-dir /workspace/downloads
+#mv t5xxl_fp8_e4m3fn.safetensors text_encoder/t5xxl_fp8_e4m3fn.safetensors
+#mv text_encoder/model.safetensors text_encoder/clip_l.safetensors
+#mv vae/diffusion_pytorch_model.safetensors vae/flux_vae.safetensors
+#touch /workspace/downloads/hf_download_1.fin) &> hf_download_1.log &
+#
+#(huggingface-cli download black-forest-labs/FLUX.1-dev flux1-dev.safetensors ae.safetensors --local-dir /workspace/downloads/unet
+#mv flux1-dev.safetensors unet/flux1-dev.safetensor
+#mv ae.safetensors vae/ae.safetensor
+#touch /workspace/downloads/hf_download_2.fin) &> hf_download_2.log &
+#
+#(huggingface-cli download XLabs-AI/flux-RealismLora lora.safetensors --local-dir /workspace/downloads
+#mv lora.safetensors lora/realism.safetensors
+#touch /workspace/downloads/hf_download_3.fin) &> hf_download_3.log &
+#
+#(huggingface-cli download InstantX/FLUX.1-dev-Controlnet-Union diffusion_pytorch_model.safetensors --local-dir /workspace/downloads/controlnet
+#mv controlnet/diffusion_pytorch_model.safetensors controlnet/Controlnet-Union.safetensors
+#touch /workspace/downloads/hf_download_4.fin) &> hf_download_4.log &
+
 
 apt update
 apt -y install vim python3-pip curl wget git-lfs
+git config --global http.version HTTP/1.1
 
-
-cat >/dev/null <<ignore
 # Install ComfyUI
+cd /workspace
 git clone --branch=v0.2.2 https://github.com/comfyanonymous/ComfyUI.git
 cd ComfyUI
 
 # Install Nodes Manager
 cd custom_nodes
 git clone https://github.com/ltdrdata/ComfyUI-Manager.git
+git clone https://github.com/SeargeDP/ComfyUI_Searge_LLM.git
+git clone https://github.com/city96/ComfyUI-GGUF.git
 cd ..
 
-python3 -m venv venv
-source venv/bin/activate
-pip install -r requirements.txt
-python3 main.py
-ignore
 
-git config --global http.version HTTP/1.1
-git clone https://github.com/lllyasviel/stable-diffusion-webui-forge.git
-
-
-cd stable-diffusion-webui-forge/models
+cd /workspace/ComfyUI/models
 rm -rf text_encoder VAE Stable-diffusion controlnet loras
 ln -s /workspace/downloads/text_encoder text_encoder
 ln -s /workspace/downloads/vae VAE
 ln -s /workspace/downloads/unet Stable-diffusion
 ln -s /workspace/downloads/controlnet controlnet
 ln -s /workspace/downloads/lora lora
-cd ..
+ln -s /workspace/downloads/llm_gguf llm_gguf
 
+cd ..
 python3 -m venv venv
 source venv/bin/activate
-pip install -r requirements_versions.txt
-./webui.sh -f --listen
+pip install -r requirements.txt
+python3 main.py --listen 
  
