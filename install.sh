d97c92d5372db449edd16c29034a8767809e4e75
terminator4088 <43856573+terminator4088@users.noreply.github.com>
2025-07-02 15:10:16 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -22,14 +22,23 @@ mkdir controlnet
 mkdir llm_gguf
 
 downloads=(
-    "comfyanonymous/flux_text_encoders t5xxl_fp8_e4m3fn.safetensors text_encoder/t5xxl_fp8_e4m3fn.safetensors"
-    "black-forest-labs/FLUX.1-dev text_encoder/model.safetensors text_encoder/clip_l.safetensors"
-    "black-forest-labs/FLUX.1-dev vae/diffusion_pytorch_model.safetensors vae/flux_vae.safetensors"
-    "black-forest-labs/FLUX.1-dev flux1-dev.safetensors unet/flux1-dev.safetensor"
-    "black-forest-labs/FLUX.1-dev ae.safetensors vae/ae.safetensor"
-    "XLabs-AI/flux-RealismLora lora.safetensors lora/realism.safetensors"
-    "InstantX/FLUX.1-dev-Controlnet-Union diffusion_pytorch_model.safetensors controlnet/Controlnet-Union.safetensors"
-    "cognitivecomputations/dolphin-2.9.4-llama3.1-8b-gguf dolphin-2.9.4-llama3.1-8b-Q6_K.gguf llm_gguf/dolphin-2.9.4-llama3.1-8b-Q6_K.gguf"
+    #"comfyanonymous/flux_text_encoders t5xxl_fp8_e4m3fn.safetensors text_encoder/t5xxl_fp8_e4m3fn.safetensors"
+    #"black-forest-labs/FLUX.1-dev text_encoder/model.safetensors text_encoder/clip_l.safetensors"
+    #"black-forest-labs/FLUX.1-dev vae/diffusion_pytorch_model.safetensors vae/flux_vae.safetensors"
+    #"black-forest-labs/FLUX.1-dev flux1-dev.safetensors unet/flux1-dev.safetensor"
+    #"black-forest-labs/FLUX.1-dev ae.safetensors vae/ae.safetensor"
+    #https://huggingface.co/Comfy-Org/flux1-kontext-dev_ComfyUI/blob/main/split_files/diffusion_models/flux1-dev-kontext_fp8_scaled.safetensors
+
+    
+    "Comfy-Org/flux1-kontext-dev_ComfyUI split_files/diffusion_models/flux1-dev-kontext_fp8_scaled.safetensors unet/flux-kontext-fp8.safetensor"
+    #https://huggingface.co/comfyanonymous/flux_text_encoders/resolve/main/clip_l.safetensors
+    #https://huggingface.co/comfyanonymous/flux_text_encoders/resolve/main/t5xxl_fp8_e4m3fn_scaled.safetensors
+    #https://huggingface.co/Comfy-Org/Lumina_Image_2.0_Repackaged/blob/main/split_files/vae/ae.safetensors
+    
+    "comfyanonymous/flux_text_encoders clip_l.safetensors unet/flux1-kontext.safetensor"
+    "comfyanonymous/flux_text_encoders t5xxl_fp8_e4m3fn_scaled.safetensors text_encoder/t5xxl_fp8_e4m3fn_scaled.safetensors"
+    "Comfy-Org/Lumina_Image_2.0_Repackaged split_files/vae/ae.safetensors vae/flux-kontext-ae.safetensor"
+    
 )
 
 download_repo_files() {
@@ -74,47 +83,18 @@ touch /workspace/download.fin ) &
 # Wait for all jobs to complete
 
 
-#mkdir text_encoder
-#mkdir vae
-#mkdir unet
-#mkdir lora
-#mkdir controlnet
-#
-#(huggingface-cli download comfyanonymous/flux_text_encoders t5xxl_fp8_e4m3fn.safetensors --local-dir /workspace/downloads
-#huggingface-cli download black-forest-labs/FLUX.1-dev text_encoder/model.safetensors vae/diffusion_pytorch_model.safetensors --local-dir /workspace/downloads
-#mv t5xxl_fp8_e4m3fn.safetensors text_encoder/t5xxl_fp8_e4m3fn.safetensors
-#mv text_encoder/model.safetensors text_encoder/clip_l.safetensors
-#mv vae/diffusion_pytorch_model.safetensors vae/flux_vae.safetensors
-#touch /workspace/downloads/hf_download_1.fin) &> hf_download_1.log &
-#
-#(huggingface-cli download black-forest-labs/FLUX.1-dev flux1-dev.safetensors ae.safetensors --local-dir /workspace/downloads/unet
-#mv flux1-dev.safetensors unet/flux1-dev.safetensor
-#mv ae.safetensors vae/ae.safetensor
-#touch /workspace/downloads/hf_download_2.fin) &> hf_download_2.log &
-#
-#(huggingface-cli download XLabs-AI/flux-RealismLora lora.safetensors --local-dir /workspace/downloads
-#mv lora.safetensors lora/realism.safetensors
-#touch /workspace/downloads/hf_download_3.fin) &> hf_download_3.log &
-#
-#(huggingface-cli download InstantX/FLUX.1-dev-Controlnet-Union diffusion_pytorch_model.safetensors --local-dir /workspace/downloads/controlnet
-#mv controlnet/diffusion_pytorch_model.safetensors controlnet/Controlnet-Union.safetensors
-#touch /workspace/downloads/hf_download_4.fin) &> hf_download_4.log &
-
-
 apt update
 apt -y install vim python3-pip curl wget git-lfs
 git config --global http.version HTTP/1.1
 
 # Install ComfyUI
 cd /workspace
-git clone --branch=v0.2.2 https://github.com/comfyanonymous/ComfyUI.git
+git clone --branch=v0.3.43 https://github.com/comfyanonymous/ComfyUI.git
 cd ComfyUI
 
 # Install Nodes Manager
 cd custom_nodes
 git clone https://github.com/ltdrdata/ComfyUI-Manager.git
-git clone https://github.com/SeargeDP/ComfyUI_Searge_LLM.git
-git clone https://github.com/city96/ComfyUI-GGUF.git
 cd ..
 
 
