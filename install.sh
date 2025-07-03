c4523c20ad8cd40caa3b125ecba5f634beb517b0
terminator4088 <43856573+terminator4088@users.noreply.github.com>
2025-07-03 13:27:03 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -22,6 +22,7 @@ cd /workspace
 
 ### Downloads
 mkdir downloads
+mkdir cache_downloads
 cd downloads
 
 mkdir text_encoder
@@ -32,15 +33,12 @@ mkdir controlnet
 mkdir llm_gguf
 
 downloads=(
-    "http https://civitai-delivery-worker-prod.5ac0637cfd0766c97916cefa3764fbdf.r2.cloudflarestorage.com/model/289798/redKFm00NSFWEditorFP8.Wtdk.safetensors?X-Amz-Expires=86400&response-content-disposition=attachment%3B%20filename%3D%22redcraftCADSUpdatedJUN29_redKKingOfHearts.safetensors%22&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=e01358d793ad6966166af8b3064953ad/20250702/us-east-1/s3/aws4_request&X-Amz-Date=20250702T145948Z&X-Amz-SignedHeaders=host&X-Amz-Signature=871241f362ddd7804ec6903608af15000529f463e2e53aca51e44147dc59329f diffusion_models/nsfw.safetensors
+    "http https://civitai-delivery-worker-prod.5ac0637cfd0766c97916cefa3764fbdf.r2.cloudflarestorage.com/model/289798/redKFm00NSFWEditorFP8.Wtdk.safetensors?X-Amz-Expires=86400&response-content-disposition=attachment%3B%20filename%3D%22redcraftCADSUpdatedJUN29_redKKingOfHearts.safetensors%22&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=e01358d793ad6966166af8b3064953ad/20250702/us-east-1/s3/aws4_request&X-Amz-Date=20250702T145948Z&X-Amz-SignedHeaders=host&X-Amz-Signature=871241f362ddd7804ec6903608af15000529f463e2e53aca51e44147dc59329f diffusion_models/nsfw.safetensors"
     "Comfy-Org/flux1-kontext-dev_ComfyUI split_files/diffusion_models/flux1-dev-kontext_fp8_scaled.safetensors diffusion_models/flux-kontext-fp8.safetensors"
     "black-forest-labs/FLUX.1-Fill-dev flux1-fill-dev.safetensor diffusion_models/flux1-fill-dev.safetensors"
-    
     "comfyanonymous/flux_text_encoders clip_l.safetensors text_encoders/flux1-kontext.safetensors"
     "comfyanonymous/flux_text_encoders t5xxl_fp8_e4m3fn_scaled.safetensors text_encoders/t5xxl_fp8_e4m3fn_scaled.safetensors"
-    
     "Comfy-Org/Lumina_Image_2.0_Repackaged split_files/vae/ae.safetensors vae/flux-kontext-ae.safetensors"
-    
     "alimama-creative/FLUX.1-dev-Controlnet-Inpainting-Beta diffusion_pytorch_model.safetensors controlnet/flux-inpaint.safetensors"
 )
 
@@ -86,7 +84,7 @@ done
 touch /workspace/download.fin ) &
 # Wait for all jobs to complete
 
-cat> /etc/apt/apt.conf.d/99mytimeout <<EOF
+cat > /etc/apt/apt.conf.d/99mytimeout <<EOF
 Acquire::http::Timeout "9";
 Acquire::https::Timeout "9";
 EOF
